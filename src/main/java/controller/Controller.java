d385597e772dd78df4113a6f2b051b4b8fba282b
BRAGINA natalia <nata.br@gmail.com>
2021-12-06 19:38:23 +0100

    initial commit

---
+++ src/main/java/controller/Controller.java
@@ -0,0 +1,19 @@
+package controller;
+
+import model.Complex;
+
+public class Controller
+{
+    enum TypeFunction {QUADRATIC};
+    enum TypeFractal {JULIA, MANDELBROT};
+    enum ColorScheme {RED, GREEN, BLUE};
+
+    private Complex constant;
+    private double coefficient;
+    private int countThreads;
+    private long timeOfExecution;
+    private String pathTxt ;
+
+    //Размер для png
+   
+}
---
+++ src/main/java/model/Complex.java
@@ -0,0 +1,63 @@
+package model;
+
+public final class Complex
+{
+    private final double re;
+    private final double im;
+    private static final Complex ZERO = new Complex(0, 0);
+    private static final Complex ONE = new Complex(1, 0) ;
+    private static final Complex I = new Complex(0, 1);
+
+    private Complex(double re, double im)
+    {
+        this.re = re;
+        this.im = im;
+    }
+
+    //============= Static fabrics ======================)
+    
+    public static Complex createComplex(double re, double im) { return new Complex(re, im); }
+
+    public static Complex getZERO() { return ZERO; }
+
+    public static Complex getONE() { return ONE; }
+
+    public static Complex getI() { return I; }
+
+    //============= Math operations ======================)
+
+    public double realPart() { return re; }
+
+    public double imaginaryPart() { return im; }
+
+    public Complex plus(Complex c) { return new Complex(re + c.re, im + c.im); }
+
+    public Complex minus(Complex c) { return new Complex(re - c.re, im - c.im); }
+
+    public Complex times(Complex c) { return new Complex(re * c.re - im * c.im, re * c.im + im * c.re); }
+
+    public Complex dividedBy(Complex c)
+    {
+        double tmp = c.re * c.re + c.im * c.im;
+        return new Complex((re * c.re + im * c.im) / tmp, (im * c.re - re * c.im) / tmp);
+    }
+
+    //============= Others ======================)
+    @Override
+    public boolean equals(Object o)
+    {
+        if (o == this) return true;
+        if (!(o instanceof Complex)) return false;
+        Complex c = (Complex) o;
+        return (Double.compare(c.re, this.re) == 0 && Double.compare(c.im, this.im) == 0);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return (31 * Double.hashCode(re) + Double.hashCode(im));    //TODO why 31 ?
+    }
+
+    @Override
+    public String toString() { return ("(" + re + " + " + im + "i)"); }
+}
