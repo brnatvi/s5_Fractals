72aec016e6f5eb0515d7aca64c5ddb14bc80c49e
BRAGINA natalia <nata.br@gmail.com>
2021-12-29 16:29:22 +0100

    tests and diagrams for statistics

---
+++ tests_statistics.sh
@@ -0,0 +1,81 @@
+
+## This is tests to obtain statistics
+
+export JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64
+mvn compile package
+
+## to variate the size of complex rectangle please change line 48 in Controller (this functionality does not provided by Cli)
+
+## Fractal Julia
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 1000 --n 1 --size 2000x1000"
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 1000 --n 2 --size 2000x1000"
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 1000 --n 3 --size 2000x1000"
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 1000 --n 4 --size 2000x1000"
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 1000 --n 5 --size 2000x1000"
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 1000 --n 6 --size 2000x1000"
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 1000 --n 7 --size 2000x1000"
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 1000 --n 8 --size 2000x1000"
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 1000 --n 9 --size 2000x1000"
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 1000 --n 10 --size 2000x1000"
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 1000 --n 16 --size 2000x1000"
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 1000 --n 20 --size 2000x1000"
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 1000 --n 32 --size 2000x1000"
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 1000 --n 50 --size 2000x1000"
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 1000 --n 64 --size 2000x1000"
+
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 500 --n 1 --size 2000x1000"
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 500 --n 2 --size 2000x1000"
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 500 --n 4 --size 2000x1000"
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 500 --n 8 --size 2000x1000"
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 500 --n 16 --size 2000x1000"
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 500 --n 32 --size 2000x1000"
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 500 --n 64 --size 2000x1000"
+
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 250 --n 1 --size 2000x1000"
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 250 --n 2 --size 2000x1000"
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 250 --n 4 --size 2000x1000"
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 250 --n 8 --size 2000x1000"
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 250 --n 16 --size 2000x1000"
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 250 --n 32 --size 2000x1000"
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 250 --n 64 --size 2000x1000"
+
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 500 --n 1 --size 4000x2000"
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 500 --n 2 --size 4000x2000"
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 500 --n 4 --size 4000x2000"
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 500 --n 8 --size 4000x2000"
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 500 --n 16 --size 4000x2000"
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 500 --n 32 --size 4000x2000"
+#mvn exec:java@exCli -Dexec.args="--type J --color B --f BQ --c -0.74543;0.11301 --t Y --it 500 --n 64 --size 4000x2000"
+
+## Fractal Mandelbrot
+mvn exec:java@exCli -Dexec.args="--type M --color B --f BQ --c -0.74543;0.11301 --t Y --it 1000 --n 1 --size 2000x1000"
+mvn exec:java@exCli -Dexec.args="--type M --color B --f BQ --c -0.74543;0.11301 --t Y --it 1000 --n 2 --size 2000x1000"
+mvn exec:java@exCli -Dexec.args="--type M --color B --f BQ --c -0.74543;0.11301 --t Y --it 1000 --n 4 --size 2000x1000"
+mvn exec:java@exCli -Dexec.args="--type M --color B --f BQ --c -0.74543;0.11301 --t Y --it 1000 --n 8 --size 2000x1000"
+mvn exec:java@exCli -Dexec.args="--type M --color B --f BQ --c -0.74543;0.11301 --t Y --it 1000 --n 16 --size 2000x1000"
+mvn exec:java@exCli -Dexec.args="--type M --color B --f BQ --c -0.74543;0.11301 --t Y --it 1000 --n 32 --size 2000x1000"
+mvn exec:java@exCli -Dexec.args="--type M --color B --f BQ --c -0.74543;0.11301 --t Y --it 1000 --n 64 --size 2000x1000"
+
+mvn exec:java@exCli -Dexec.args="--type M --color B --f BQ --c -0.74543;0.11301 --t Y --it 500 --n 1 --size 2000x1000"
+mvn exec:java@exCli -Dexec.args="--type M --color B --f BQ --c -0.74543;0.11301 --t Y --it 500 --n 2 --size 2000x1000"
+mvn exec:java@exCli -Dexec.args="--type M --color B --f BQ --c -0.74543;0.11301 --t Y --it 500 --n 4 --size 2000x1000"
+mvn exec:java@exCli -Dexec.args="--type M --color B --f BQ --c -0.74543;0.11301 --t Y --it 500 --n 8 --size 2000x1000"
+mvn exec:java@exCli -Dexec.args="--type M --color B --f BQ --c -0.74543;0.11301 --t Y --it 500 --n 16 --size 2000x1000"
+mvn exec:java@exCli -Dexec.args="--type M --color B --f BQ --c -0.74543;0.11301 --t Y --it 500 --n 32 --size 2000x1000"
+mvn exec:java@exCli -Dexec.args="--type M --color B --f BQ --c -0.74543;0.11301 --t Y --it 500 --n 64 --size 2000x1000"
+
+mvn exec:java@exCli -Dexec.args="--type M --color B --f BQ --c -0.74543;0.11301 --t Y --it 250 --n 1 --size 2000x1000"
+mvn exec:java@exCli -Dexec.args="--type M --color B --f BQ --c -0.74543;0.11301 --t Y --it 250 --n 2 --size 2000x1000"
+mvn exec:java@exCli -Dexec.args="--type M --color B --f BQ --c -0.74543;0.11301 --t Y --it 250 --n 4 --size 2000x1000"
+mvn exec:java@exCli -Dexec.args="--type M --color B --f BQ --c -0.74543;0.11301 --t Y --it 250 --n 8 --size 2000x1000"
+mvn exec:java@exCli -Dexec.args="--type M --color B --f BQ --c -0.74543;0.11301 --t Y --it 250 --n 16 --size 2000x1000"
+mvn exec:java@exCli -Dexec.args="--type M --color B --f BQ --c -0.74543;0.11301 --t Y --it 250 --n 32 --size 2000x1000"
+mvn exec:java@exCli -Dexec.args="--type M --color B --f BQ --c -0.74543;0.11301 --t Y --it 250 --n 64 --size 2000x1000"
+
+
+
+
+
+
+
+
